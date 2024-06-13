868264f05eb1f11abc606b156034b7eba75e1e37
pibebtol <Julius.felchow@googlemail.com>
2024-06-14 00:04:39 +0200

    remove deno stuff

--- main.ts
+++
@@ -1,9 +0,0 @@
-import abcjs from 'abcjs';
-
-abcjs.renderAbc("paper", "X:1\nK:D\nDD AA|BBA2|\n");
-
-console.log(add(3, 5));
-
-export function add(a: number, b: number): number {
-  return a + b;
-}
--- main_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertEquals } from "std/assert/mod.ts";
-import { add } from "./main.ts";
-
-Deno.test("sayHello function", () => {
-  assertEquals(5, add(2, 3));
-});
